ce5e6428f400ab14807780598fea77494049c72b
HieuDuong <duonghieu.7104@gmail.com>
2025-05-08 07:16:33 +0700

    up

--- streamlit/video_processor.py
+++ streamlit/video_processor.py
@@ -10,7 +10,7 @@ model_dir = os.path.join(current_dir, 'model')  # Thư mục chứa .onnx và sv
 
 # Tải mô hình SVM
 svc = joblib.load(os.path.join(model_dir, 'svc.pkl'))
-mydict = ['DaiLong', 'MinhHieu', 'VanThang', 'caothang']
+mydict = ['DaiLong' 'MinhHieu' 'MinhHoang' 'VanThang' 'caothang']
 
 class VideoProcessor:
     def __init__(self):
